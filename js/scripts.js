a75d05310d7725612073e934830f75d9cc97f454
piekario13 <piekario13@gmail.com>
2017-10-26 21:38:05 +0200

    kodilla_triangleArea

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter first number');
+var h = prompt('Enter second number');
+var triangleArea = a*h/2
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
